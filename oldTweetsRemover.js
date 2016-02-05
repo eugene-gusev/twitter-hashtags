30240cfe1f33e847dc948a5d0e8688168bdbe02b
Eugene Gusev <zhenya_96@inbox.ru>
2016-02-05 19:55:24 +0300

    no message

--- oldTweetsRemover.js
+++ oldTweetsRemover.js
@@ -4,9 +4,9 @@
 function clean (db) {
     var date = new Date();
     var time = date.getTime();
-    var limits = [time-86400000,
-                  time-259200000,
-                  time-604800000];
+    var limits = [time-86400000,    //сутки
+                  time-259200000,   //3 дня
+                  time-604800000];  //неделя
     var cursors=[db.collection('tweets_0').find({date: {$lte: limits[0]}}),
                  db.collection('tweets_1').find({date: {$lte: limits[1]}}),
                  db.collection('tweets_2').find({date: {$lte: limits[2]}})];
@@ -40,7 +40,7 @@ function clean (db) {
                         }
                     });
 
-                    if (i!=2) {
+                    if (i!=2) { //если твиты не старше 7 дней
                         db.collection('tweets_' + (i + 1)).insert(objs, function (err, result) {
                             if (err) {
                                 console.log(err);
